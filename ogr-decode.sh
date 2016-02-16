068fa1eb852d68a7821a17b248588810726bb857
D-ECHO <cover-mail@t-online.de>
2016-02-16 16:15:57 +0100

    Update airports

--- ogr-decode.sh
+++ ogr-decode.sh
@@ -3,3 +3,6 @@ ogr-decode --max-segment 500 --line-width 1 --area-type Sand work/Sand data/shap
 ogr-decode --max-segment 500 --area-type Airport work/Airport data/shapefiles/cs_airport
 ogr-decode --max-segment 500 --area-type Default work/Default data/shapefiles/osm_landmass
 ogr-decode --max-segment 500 --area-type Town work/Town data/shapefiles/cs_town
+ogr-decode --max-segment 500 --area-type Industrial work/Industrial data/shapefiles/cs_industrial
+ogr-decode --max-segment 500 --area-type Estuary work/Estuary data/shapefiles/cs_estuary
+ogr-decode --max-segment 500 --area-type GrassCover work/GrassCover data/shapefiles/cs_grass
--- tg-construct.sh
+++ tg-construct.sh
@@ -1,2 +1,2 @@
-tg-construct --work-dir=./work --output-dir=./output/Terrain --min-lon=71 --max-lon=74 --min-lat=-1 --max-lat=7 SRTM-3 RainForest AirportArea AirportObj Default Town Sand Airport 
+tg-construct --work-dir=./work --output-dir=./output/Terrain --min-lon=71 --max-lon=74 --min-lat=-1 --max-lat=7 SRTM-3 RainForest AirportArea AirportObj Default Town Sand Airport Industrial Estuary GrassCover
 
