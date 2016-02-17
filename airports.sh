dda0bdca302a71d4f03b0d2c4cd73b9e02449c4d
D-ECHO <cover-mail@t-online.de>
2016-02-17 16:18:19 +0100

    Add VRMO, developed more islands!

--- airports.sh
+++ airports.sh
@@ -3,3 +3,4 @@ genapts850 --input=data/airports/vrmh.dat --work=./work
 genapts850 --input=data/airports/vrmk.dat --work=./work
 genapts850 --input=data/airports/vrmm.dat --work=./work
 genapts850 --input=data/airports/vrmt.dat --work=./work
+genapts850 --input=data/airports/vrmo.dat --work=./work
